bd833f6260a20a58bbe02419b31c0a35e0eed463
jeehoo0767 <66991772+jeehoo0767@users.noreply.github.com>
2020-08-06 17:50:15 +0900

    javascript

---
+++ JS04/script.js
@@ -0,0 +1,10 @@
+function ch() { document.getElementById("a").innerHTML = "<a href='https://www.nate.com'>네이트</a>";
+}
+
+function chcss(){
+    document.getElementById("a").style.color="red";
+    document.getElementById("a").style.border="1px solid black"
+    
+    document.getElementById("a").classList.add("hide");
+   
+}
